c7cf073e18f808024483a486857ab71416808f7f
Yuning Shi <shiyuning@gmail.com>
2023-12-06 09:18:24 -0500

    :sparkles: Change to 20 soil layers

--- src/initialize.c
+++ src/initialize.c
@@ -20,23 +20,11 @@ void Initialize(cycles_struct *cycles, N_Vector CV_Y, void **cvode_mem)
 
     ctrl->stepsize = STEPSIZE;
 
-    phys->zsoil[0] = -0.1;
-    phys->zsoil[1] = -0.4;
-    phys->zsoil[2] = -1.0;
-    phys->zsoil[3] = -2.0;
-
-    phys->soil_depth[0] = 0.1;
-    phys->soil_depth[1] = 0.3;
-    phys->soil_depth[2] = 0.6;
-    phys->soil_depth[3] = 1.0;
-
-    ws->smc[0] = 0.3568;
-    ws->smc[1] = 0.3601;
-    ws->smc[2] = 0.3669;
-    ws->smc[3] = 0.3793;
-
     for (kz = 0; kz < NSOIL; kz++)
     {
+        phys->zsoil[kz] = -0.1 * (kz + 1);
+        phys->soil_depth[kz] = 0.1;
+        ws->smc[kz] = (kz < 2) ? 0.3568 : 0.464;
         ws->potential[kz] = SoilWaterPot(soil->porosity[kz], soil->air_entry_pot[kz], soil->b[kz], ws->smc[kz]);
     }
 
--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     SUNLinearSolver sun_ls;
     cycles_struct   cycles;
     int             kstep;
+    int             kz;
     FILE           *fp;
 
 #if defined(unix) || defined(__unix__) || defined(__unix)
@@ -35,13 +36,20 @@ int main(int argc, char *argv[])
 
     // Open output file and write header
     fp = fopen("cycles.txt", "w");
-    fprintf(fp, "step,smc0,smc1,smc2,smc3,psi0,psi1,psi2,psi3\n");
+    fprintf(fp, "step");
+    for (kz = 0; kz < NSOIL; kz++)
+    {
+        fprintf(fp, ",smc%d", kz);
+    }
+    for (kz = 0; kz < NSOIL; kz++)
+    {
+        fprintf(fp, ",psi%d", kz);
+    }
+    fprintf(fp, "\n");
 
     // Run model
     for (kstep = 0; kstep < NSTEPS; kstep++)
     {
-        int             kz;
-
         // Run soil moisture time step
         SWC(kstep, &cycles, cvode_mem, CV_Y);
 
@@ -53,7 +61,7 @@ int main(int argc, char *argv[])
         }
         for (kz = 0; kz < NSOIL; kz++)
         {
-            fprintf(fp, ",%.2lf", cycles.ws.potential[kz]);
+            fprintf(fp, ",%.3lf", cycles.ws.potential[kz]);
         }
         fprintf(fp, "\n");
     }
